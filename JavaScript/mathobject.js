61699c15e51b54dc5fa5b22df7eb279b72b75c39
KirishaK <136303274+KirishaK@users.noreply.github.com>
2024-07-18 08:42:53 +0530

    Add files via upload

---
+++ JavaScript/mathobject.js
@@ -0,0 +1,16 @@
+
+// Use Math methods for calculations
+let squareRoot = Math.sqrt(16);
+console.log(`Square root of 16: ${squareRoot}`);
+
+let power = Math.pow(2, 3);
+console.log(`2 to the power of 3: ${power}`);
+
+let ceil = Math.ceil(3.2);
+console.log(`Ceil of 3.2: ${ceil}`);
+
+/*
+Random number: 4
+Square root of 16: 4
+2 to the power of 3: 8
+Ceil of 3.2: 4*/
---
+++ JavaScript/pop.js
@@ -0,0 +1,9 @@
+let arr= [10,20,30,40,50];
+let value= arr.pop();
+console.log(value);
+console.log(arr)
+
+/*output
+50
+[ 10, 20, 30, 40 ]
+ */
